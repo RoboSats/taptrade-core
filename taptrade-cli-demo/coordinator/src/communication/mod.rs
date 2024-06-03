83a2c4b812291b771b19a52ce1fdb11ce31db8c4
f321x <f321x@tutamail.com>
2024-06-03 20:51:30 +0200

    add comments

--- taptrade-cli-demo/coordinator/src/communication/mod.rs
+++ taptrade-cli-demo/coordinator/src/communication/mod.rs
@@ -9,3 +9,7 @@ fn index() -> &'static str {
 pub fn webserver() -> Rocket<build> {
     rocket::build().mount("/", routes![index])
 }
+
+// serde to parse json
+// https://www.youtube.com/watch?v=md-ecvXBGzI  BDK + Webserver video
+// https://github.com/tokio-rs/axum
